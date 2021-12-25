2aa402ba439b5e537320b6eb356421fbde1d292b
thankarezos <thankarezos@gmail.com>
2021-12-25 02:35:12 +0200

    connect to database

--- ermisTv/src/main/java/gr/ihu/ermistv/App.java
+++ ermisTv/src/main/java/gr/ihu/ermistv/App.java
@@ -33,8 +33,17 @@ public class App extends Application {
         return fxmlLoader.load();
     }
 
-    public static void main(String[] args) {
-//        Connection db = DriverManager.getConnection(url, username, password);
+    public static void main(String[] args){
+        Connection c = null;
+      try {
+         Class.forName("org.postgresql.Driver");
+         c = DriverManager.getConnection(credentials.url, credentials.user, credentials.pass);
+      } catch (Exception e) {
+         e.printStackTrace();
+         System.err.println(e.getClass().getName()+": "+e.getMessage());
+         System.exit(0);
+      }
+      System.out.println("Opened database successfully");
         launch();
     }
 
