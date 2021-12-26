02e3def152c09dca62d63340dc40962a27cdc250
Periklis-Chr <It175073@it.teithe.gr>
2021-12-26 04:43:27 +0200

    credentials EMPTY

---
+++ ermisTv/src/main/java/gr/ihu/ermistv/credentials.java
@@ -0,0 +1,7 @@
+package gr.ihu.ermistv;
+
+public class credentials {
+    static String user = "user";
+    static String pass = "pass";
+    static String url = "jdbc:postgresql://dblabs.it.teithe.gr:5432/-USERNAME-";
+}
