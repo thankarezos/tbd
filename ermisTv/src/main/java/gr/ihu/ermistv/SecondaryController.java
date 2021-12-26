38624da1599dd1a19d6b6cf42ac881569726fec6
Periklis-Chr <It175073@it.teithe.gr>
2021-12-26 09:01:02 +0200

    program coise box

--- ermisTv/src/main/java/gr/ihu/ermistv/SecondaryController.java
+++ ermisTv/src/main/java/gr/ihu/ermistv/SecondaryController.java
@@ -1,16 +1,28 @@
 package gr.ihu.ermistv;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class SecondaryController {
+public class SecondaryController implements Initializable {
     @FXML
     private AnchorPane secondary;
+    @FXML
+    private ChoiceBox choise_day,choise_type;
+    @FXML
+    private Label label_test;
 
     @FXML
     private void minimizedWindow(MouseEvent event) {
@@ -26,5 +38,26 @@ public class SecondaryController {
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("fxml/login_view");
+    }
+    private String[] dayC ={"monday","tuesday","wednesday","thrusday","friday","saturday","sunday"};
+    private String[] typeC ={"movie","series","broadcast","documentary","NEWS"};
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        choise_day.getItems().addAll(dayC);
+        choise_day.setOnAction(this::getDay);
+
+        choise_type.getItems().addAll(typeC);
+        choise_type.setOnAction(this::geType);
+    }
+
+    private void geType(Event event) {
+        String type = (String) choise_type.getValue();
+        label_test.setText(type);
+    }
+
+    public void getDay(Event event){
+        String day = (String) choise_day.getValue();
+        label_test.setText(day);
     }
 }
