3334d0b5ad3b26f8371ef24f8ec8bdcb193eb651
thankarezos <thankarezos@gmail.com>
2022-01-02 22:32:25 +0200

    program

--- ermisTv/src/main/java/gr/ihu/ermistv/controller/Program_Controller.java
+++ ermisTv/src/main/java/gr/ihu/ermistv/controller/Program_Controller.java
@@ -1,6 +1,246 @@
 package gr.ihu.ermistv.controller;
 
-public class Program_Controller {
+import java.net.URL;
+import java.sql.Time;
+import java.util.ResourceBundle;
+import javafx.event.EventType;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+
+public class Program_Controller implements Initializable{
+    
+    @FXML
+    private VBox emptypane, time, program;
+    
+    private String[] days = {"Monday","Tuesday","Thurstday","Wednesday","Friday","Saturday","Sunday"};
+    private String[] colors = {"white","grey","Thurstday","Wednesday","Friday","Saturday","Sunday"};
+    private int daysize = 130;
+    private int emptyS = 15;
+    private double spaces = 5;
+    private double spacesH = 10;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        
+        program.setLayoutX(daysize + spacesH);
+        emptypane.setSpacing(spaces);
+        time.setSpacing(spaces);
+        AnchorPane.setTopAnchor(time, emptyS -((double)emptyS/2 - spaces/2));
+        int t;
+        
+//        HBox Hbox1 = new HBox();
+//        Hbox1.setPrefHeight(emptyS/2 - spaces);
+//        Hbox1.setPrefHeight(0);
+//        time.getChildren().add(Hbox1);
+        String panecolor = "";
+        for(int i=1; i<=7;i++){
+            if(i%2 == 0){
+                panecolor =  "-fx-background-color:grey"; 
+            }
+            else{
+                panecolor =  "-fx-background-color:#f6f7f9";
+               
+            }
+    //        boolean half = true;
+    //        int timeS = 1;
+    //        String timeSet = "00:30";
+            t = 30;
+            
+            HBox dayBox = new HBox();
+            Text dayText = new Text ();
+            dayText.setStyle("-fx-font-size:25px");
+            dayText.setText(days[i - 1]);
+            dayBox.getChildren().add(dayText);
+            dayBox.setStyle("-fx-background-color:red");
+            dayBox.setPrefWidth(daysize);
+            dayBox.setPrefHeight(30);
+            
+            for(int j = 1; j <=47;j++){
+
+                    HBox hbox = new HBox();
+                    hbox.setSpacing(spacesH);
+                    hbox.setMinHeight(Region.USE_PREF_SIZE);
+
+                    HBox day = new HBox();
+                    day.setPrefWidth(daysize);
+                    day.setPrefHeight(emptyS);
+                    day.setMinHeight(Region.USE_PREF_SIZE);
+                    day.setMinWidth(Region.USE_PREF_SIZE);
+                    if(dayBox != null){
+                        day.getChildren().add(dayBox);
+                        dayBox = null;
+                    }
+
+
+                    Pane pane = new Pane();
+                    pane.setPrefHeight(emptyS);
+                    pane.setMinHeight(Region.USE_PREF_SIZE);
+                    pane.setStyle(panecolor);
+                    HBox.setHgrow(pane, Priority.ALWAYS);
+
+                    HBox Htime = new HBox();
+                    Htime.setPrefHeight(emptyS);
+                    Htime.setMinHeight(Region.USE_PREF_SIZE);
+                    Htime.setStyle("-fx-background-color:red");
+                    Htime.setAlignment(Pos.CENTER);
+                    Text text = new Text();
+                    int hours = t / 60; //since both are ints, you get an int
+                    int minutes = t % 60;
+                    text.setText(String.format("%02d", hours) + ":" + String.format("%02d", minutes));
+                    t += 30;
+                    Htime.getChildren().add(text);
+
+
+                    text.getStyleClass().add("textname");
+
+                    hbox.getChildren().addAll(day,pane);
+                    time.getChildren().add(Htime);
+                    emptypane.getChildren().add(hbox);
+            }
+            HBox hbox = new HBox();
+            hbox.setSpacing(spacesH);
+            hbox.setMinHeight(Region.USE_PREF_SIZE);
+
+            HBox day = new HBox();
+            day.setPrefWidth(daysize);
+            day.setPrefHeight(emptyS);
+            day.setMinHeight(Region.USE_PREF_SIZE);
+            day.setMinWidth(Region.USE_PREF_SIZE);
+
+
+            Pane pane = new Pane();
+            pane.setPrefHeight(emptyS);
+            pane.setMinHeight(Region.USE_PREF_SIZE);
+            pane.setStyle(panecolor);
+            HBox.setHgrow(pane, Priority.ALWAYS);
+
+            HBox Htime = new HBox();
+            Htime.setPrefHeight(emptyS);
+            Htime.setMinHeight(Region.USE_PREF_SIZE);
+            Htime.setStyle("-fx-background-color:red");
+            Htime.setAlignment(Pos.CENTER);
+            Text text = new Text();
+            text.setText("00:00");
+            Htime.getChildren().add(text);
+
+
+            text.getStyleClass().add("textname");
+
+            hbox.getChildren().addAll(day,pane);
+            time.getChildren().add(Htime);
+            emptypane.getChildren().add(hbox);
+        }
+        t = 30;
+        for(int j = 1; j <=10;j++){
+
+                    HBox hbox = new HBox();
+                    hbox.setSpacing(spacesH);
+                    hbox.setMinHeight(Region.USE_PREF_SIZE);
+
+                    HBox day = new HBox();
+                    day.setPrefWidth(daysize);
+                    day.setPrefHeight(emptyS);
+                    day.setMinHeight(Region.USE_PREF_SIZE);
+                    day.setMinWidth(Region.USE_PREF_SIZE);
+
+
+                    Pane pane = new Pane();
+                    pane.setPrefHeight(emptyS);
+                    pane.setMinHeight(Region.USE_PREF_SIZE);
+                    pane.setStyle(panecolor);
+                    HBox.setHgrow(pane, Priority.ALWAYS);
+
+                    HBox Htime = new HBox();
+                    Htime.setPrefHeight(emptyS);
+                    Htime.setMinHeight(Region.USE_PREF_SIZE);
+                    Htime.setStyle("-fx-background-color:red");
+                    Htime.setAlignment(Pos.CENTER);
+                    Text text = new Text();
+                    int hours = t / 60; //since both are ints, you get an int
+                    int minutes = t % 60;
+                    text.setText(String.format("%02d", hours) + ":" + String.format("%02d", minutes));
+                    t += 30;
+                    Htime.getChildren().add(text);
+
+
+                    text.getStyleClass().add("textname");
+
+                    hbox.getChildren().addAll(day,pane);
+                    time.getChildren().add(Htime);
+                    emptypane.getChildren().add(hbox);
+        }
+        
+        
+        
+        
+//                HBox hbox = new HBox();
+//                
+//                
+//                Pane pane1 = new Pane();
+//                pane1.setPrefWidth(100);
+//                
+//                hbox.getChildren().add(pane1);
+//                hbox.setMinHeight(Region.USE_PREF_SIZE);
+//                
+//                Pane pane = new Pane();
+//                pane.setPrefHeight(15);
+//                HBox.setHgrow(pane, Priority.ALWAYS);
+//                pane.setMinHeight(Region.USE_PREF_SIZE);
+//                pane.setStyle("-fx-background-color:white");
+//                
+//                hbox.getChildren().add(pane);
+//                emptypane.getChildren().add(hbox);
+//
+//                HBox Hbox = new HBox();
+//                Hbox.setPrefHeight(15);
+//                Hbox.setMinHeight(Region.USE_PREF_SIZE);
+//                Hbox.setStyle("-fx-background-color:red");
+//                Hbox.setAlignment(Pos.CENTER);
+//                Text text = new Text();
+//                text.setText("00:00");
+//                t = 30;
+//
+//
+//                text.getStyleClass().add("textname");
+//                Hbox.getChildren().add(text);
+//                time.getChildren().add(Hbox);
+//        }
+//        t = 30;
+//        for(int j = 1; j <=10;j++){
+//                Pane pane = new Pane();
+//                pane.setPrefHeight(15);
+//                pane.setMinHeight(Region.USE_PREF_SIZE);
+//                pane.setStyle("-fx-background-color:white");
+//                emptypane.getChildren().add(pane);
+//
+//                HBox Hbox = new HBox();
+//                Hbox.setPrefHeight(15);
+//                Hbox.setMinHeight(Region.USE_PREF_SIZE);
+//                Hbox.setStyle("-fx-background-color:red");
+//                Hbox.setAlignment(Pos.CENTER);
+//                Text text = new Text();
+//                int hours = t / 60; //since both are ints, you get an int
+//                int minutes = t % 60;
+//                text.setText(String.format("%02d", hours) + ":" + String.format("%02d", minutes));
+//                t += 30;
+//
+//
+//                text.getStyleClass().add("textname");
+//                Hbox.getChildren().add(text);
+//                time.getChildren().add(Hbox);
+//        }
+        
+        
+    }
 
 
 }
