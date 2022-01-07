6559edba925c9c89b48fd02defa50c7105c7b91e
thankarezos <thankarezos@gmail.com>
2022-01-07 16:11:48 +0200

    libaries

--- ermisTv/jars/jsch/module-info.java
+++
@@ -1,9 +0,0 @@
-module jsch {
-    requires java.security.jgss;
-
-    exports com.jcraft.jsch;
-    exports com.jcraft.jsch.jce;
-    exports com.jcraft.jsch.jcraft;
-    exports com.jcraft.jsch.jgss;
-
-}
